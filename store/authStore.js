5910116d9e12bff1baf27d664f7eaca3a243d1c6
tig <leehjpp4@gmail.com>
2026-03-27 16:41:05 +0900

    feat (authStore) :  AccessToken을 store로 관리

---
+++ store/authStore.js
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+
+const useAuthStore = create((set)=> ({
+    accessToken: null, 
+    setAccessToken: (token) => set({ accessToken:token}),
+    logout: () => set ({ accessToken: null }),
+}));
+
+export default useAuthStore;
