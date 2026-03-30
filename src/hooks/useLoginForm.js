fa35be93d66f8ad9125e2b78a9122682658d286a
tig <leehjpp4@gmail.com>
2026-03-30 15:14:58 +0900

    refactor : useLoginForm 공통 인스턴스 임포트후 그에 맞게 로그인 로직 수정

--- src/hooks/useLoginForm.js
+++ src/hooks/useLoginForm.js
@@ -1,29 +1,40 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"
-// 공통 인스턴스 머지후 임포트해야함
+import { useNavigate } from "react-router-dom";
+import api from "../api/instance"; 
+import useAuthStore from "../../store/authStore";
 
 export const useLoginForm = () => {
     const navigate = useNavigate();
-    const [formData, setFormData] = useState({ login_id: "", password: ""});
+    const { setAccessToken } = useAuthStore(); 
+    const [formData, setFormData] = useState({ login_id: "", password: "" });
 
     const handleChange = (e) => {
-        const {name ,value} = e.target;
-        setFormData((prev) => ({ ...prev,[name]: value }))
-    }
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    };
 
     const handleLogin = async (e) => {
-    e.preventDefault();
-    try{
-        const data = await login(formData.login_id, formData.password);
-        if (data.accessToken) {
-                localStorage.setItem("accessToken", data.accessToken);
-                alert("로그인 성공");
-                navigate("/dashboard"); 
-    } 
+        e.preventDefault();
+        try {
+            const response = await api.post("/login", {
+                loginId: formData.login_id,
+                password: formData.password
+            });
 
-} catch (error){
-    alert("아이디 또는 비밀번호를 확인하세요.");
-}
+            const data = response.data;
+
+            if (data.accessToken) {
+
+                setAccessToken(data.accessToken); 
+                
+                alert("로그인 성공");
+                navigate("/dashboard");
+            }
+        } catch (error) {
+            console.error("로그인 에러:", error);
+            alert("아이디 또는 비밀번호를 확인하세요.");
+        }
     };
-    return {formData, handleChange, handleLogin};
+
+    return { formData, handleChange, handleLogin };
 };
