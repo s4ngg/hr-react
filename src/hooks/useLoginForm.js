dd42738465a273ec3e2ea237aa94823af35c8f38
tig <leehjpp4@gmail.com>
2026-03-30 16:28:22 +0900

    refactor : loginQuery 와 useLoginForm으로 기능을 분배하였습니다

--- src/hooks/useLoginForm.js
+++ src/hooks/useLoginForm.js
@@ -1,40 +1,20 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import api from "../api/instance"; 
-import useAuthStore from "../../store/authStore";
+import { useLoginMutation } from "../query/loginQuery"; 
 
 export const useLoginForm = () => {
-    const navigate = useNavigate();
-    const { setAccessToken } = useAuthStore(); 
     const [formData, setFormData] = useState({ login_id: "", password: "" });
+    const { mutate: login, isLoading } = useLoginMutation();
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData((prev) => ({ ...prev, [name]: value }));
     };
 
-    const handleLogin = async (e) => {
+    const handleLogin = (e) => {
         e.preventDefault();
-        try {
-            const response = await api.post("/login", {
-                loginId: formData.login_id,
-                password: formData.password
-            });
-
-            const data = response.data;
-
-            if (data.accessToken) {
-
-                setAccessToken(data.accessToken); 
-                
-                alert("로그인 성공");
-                navigate("/dashboard");
-            }
-        } catch (error) {
-            console.error("로그인 에러:", error);
-            alert("아이디 또는 비밀번호를 확인하세요.");
-        }
+        
+        login(formData); 
     };
 
-    return { formData, handleChange, handleLogin };
+    return { formData, handleChange, handleLogin, isLoading };
 };
