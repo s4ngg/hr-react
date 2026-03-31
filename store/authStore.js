d265936435c836c8e9b5f4e3a62935dc8bccae74
tig <leehjpp4@gmail.com>
2026-03-31 11:20:27 +0900

    feat : DTO에서 받는 정보들을 로그인할때 더 정보를 담을 수 있도록 필드 추가

--- store/authStore.js
+++ store/authStore.js
@@ -3,8 +3,26 @@ import { create } from "zustand";
 
 const useAuthStore = create((set)=> ({
     accessToken: null, 
-    setAccessToken: (token) => set({ accessToken:token}),
-    logout: () => set ({ accessToken: null }),
+    user: null,
+
+    setAuth: (data) => 
+        set({
+            accessToken: data.token,
+            user:{
+                memberId: data.memberId,
+                employeeNo: data.employeeNo,
+                name: data.name,
+                role: data.role,
+                deptName: data.deptName,
+                departmentId: data.departmentId,
+            },
+        }),
+
+        logout: () =>
+            set({
+                accessToken: null,
+                user: null,
+            }),
 }));
 
 export default useAuthStore;
