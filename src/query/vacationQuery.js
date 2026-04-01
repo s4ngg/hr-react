ad8dff4c63ce8cd326d003d5bca0b449064c3201
tig <leehjpp4@gmail.com>
2026-04-01 10:01:26 +0900

    feat: 휴가관련 Query

---
+++ src/query/vacationQuery.js
@@ -0,0 +1,36 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import api from "../api/instance";
+
+//내 휴가 내역 조회
+export const useMyVacationHistory = (memberId) =>{
+    return useQuery({
+        queryKey: ['myVacationHistory', memberId],
+        queryFn: async() =>{
+            const res = await api.get(`/vacations/my/${memberId}`);
+            return res.data;
+        },
+        enabled: !!memberId,
+    })
+}
+
+// 휴가 신청
+export const useRequestVacation = (token) =>{
+    const queryClient = useQueryClient();
+
+
+    return useMutation({
+
+    mutationFn: async (vacationData) => {
+    
+    const res = await api.post('/vacations', vacationData);
+    return res.data;
+    },
+    onSuccess: () => {
+    queryClient.invalidateQueries({ queryKey: ['myVacationHistory', memberId] });
+    alert("휴가 신청이 완료되었습니다!");
+    },
+    onError: (error) => {
+    console.error("신청 실패:", error);
+    }
+});
+}
